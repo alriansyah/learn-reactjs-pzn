ae3017d9ec22df6b38702a4f8771aef32918c7d6
alriansyah <alriansyah1218@gmail.com>
2025-08-19 11:08:44 +0700

    feat: learn react js -> event object

--- 1-reactjs-dasar/src/components/4-button/AlertButton.tsx
+++ 1-reactjs-dasar/src/components/4-button/AlertButton.tsx
@@ -1,10 +1,13 @@
+import React from "react";
+
 interface AlertButtonProps {
   text: string;
   message: string;
 }
 
 function AlertButton({ text, message }: AlertButtonProps) {
-  function handleClick(): void {
+  function handleClick(e: React.MouseEvent<HTMLButtonElement>): void {
+    console.log(e);
     alert(message);
   }
 
