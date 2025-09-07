d798104c5907aafd5b4cf0d41678980147cc9f79
alriansyah <alriansyah1218@gmail.com>
2025-09-07 23:50:54 +0700

    feat: learn react js redux toolkit -> action reducer and dispatch action

--- 3-reactjs-redux/src/components/1-counter/index.tsx
+++ 3-reactjs-redux/src/components/1-counter/index.tsx
@@ -1,12 +1,36 @@
-import { useSelector } from "react-redux";
-import type { RootState } from "../../redux/store";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../../redux/counterSlice";
+import type { RootState, AppDispatch } from "../../redux/store";
 
 function Counter() {
   const counter = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
+  function handleIncrement(): void {
+    dispatch(increment());
+  }
+
+  function handleDecrement(): void {
+    dispatch(decrement());
+  }
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-4">
       <h1 className="text-3xl font-bold">Counter : {counter}</h1>
+      <div className="flex gap-1">
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+          onClick={handleIncrement}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded"
+          onClick={handleDecrement}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
